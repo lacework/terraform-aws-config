ea104197101069cdfddf510973c5e75223d89fc4
Darren Murray <darren.murray@lacework.net>
2020-12-10 15:15:35 +0000

    build(dependency): Enable minor versions for aws-iam-role dependency constaint
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ locals {
 
 module "lacework_cfg_iam_role" {
   source                  = "lacework/iam-role/aws"
-  version                 = "~> 0.1"
+  version                 = "~> 0.1.0"
   create                  = var.use_existing_iam_role ? false : true
   iam_role_name           = var.iam_role_name
   lacework_aws_account_id = var.lacework_aws_account_id
