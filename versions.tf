46dc1e862b7a60d65c0254f2b64ec656db948519
Scott Ford <scott.ford@lacework.net>
2021-04-28 07:04:36 -0700

    chore: bump version of required Terraform version to 0.12.26 and required version of Lacework provider to 0.3
    Signed-off-by: Scott Ford <scott.ford@lacework.net>

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = ">= 0.12.26"
 
   required_providers {
     aws    = "~> 3.0"
@@ -7,7 +7,7 @@ terraform {
     time   = "~> 0.6"
     lacework = {
       source  = "lacework/lacework"
-      version = "~> 0.2"
+      version = "~> 0.3"
     }
   }
 }
