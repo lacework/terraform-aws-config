2131806416f0417a45b43fe106b2e474c9bce6df
naseemkullah <24660299+naseemkullah@users.noreply.github.com>
2022-03-10 13:33:54 -0500

    feat: support aws provider 4.x (#39)

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12.31"
 
   required_providers {
-    aws    = "~> 3.35"
+    aws    = ">= 3.35.0, < 5.0.0"
     random = ">= 2.1"
     time   = "~> 0.6"
     lacework = {
