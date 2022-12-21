c514994c3a6511e4f055e2ccc70c9a30c1bda58e
Salim Afiune <afiune@lacework.net>
2022-12-21 11:45:11 -0800

    fix: use valid module argument permission_boundary_arn

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "lacework_cfg_iam_role" {
   version                 = "~> 0.3"
   create                  = var.use_existing_iam_role ? false : true
   iam_role_name           = var.iam_role_name
-  permissions_boundary    = var.permission_boundary_arn
+  permission_boundary_arn = var.permission_boundary_arn
   lacework_aws_account_id = var.lacework_aws_account_id
   external_id_length      = var.external_id_length
   tags                    = var.tags
