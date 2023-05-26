5f9cd7d2c40427a6b043fa239e074b4e6971165f
John Robison <jrobison@springbuk.com>
2023-05-26 11:53:01 -0400

    [DEV-2665] support AWS provider v5

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.14"
 
   required_providers {
-    aws    = ">= 3.35.0, < 5.0.0"
+    aws    = ">= 3.35.0, < 6.0.0"
     random = ">= 2.1"
     time   = "~> 0.6"
     lacework = {
