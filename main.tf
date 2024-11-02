59552a7674e69fef808f1220a61655293e43a0de
Lei Jin <lei.jin@lacework.net>
2024-11-02 00:20:01 +0000

    fix(LINK-3087): Add permission in StepFunctions
    Signed-off-by: Lei Jin <lei.jin@lacework.net>

--- main.tf
+++ main.tf
@@ -149,6 +149,13 @@ data "aws_iam_policy_document" "lacework_audit_policy" {
     ]
     resources = ["*"]
   }
+
+  statement {
+    sid = "STATES"
+    actions = ["states:ListTagsForResource",
+    ]
+    resources = ["*"]
+  }
 }
 
 resource "aws_iam_policy" "lacework_audit_policy" {
